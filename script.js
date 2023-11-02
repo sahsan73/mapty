5eff972410e89c428bab7e0d1292a5385b792071
Saquib Ahsan <saquibahsan73@gmail.com>
2023-11-02 10:57:46 +0530

    add map using leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// console.log(navigator.geolocation);
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { latitude, longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+
+    /* The map method accepts an "id" of an html element inside which the
+     * map will be rendered. And "L" is a namespace of Leaflet library, just
+     * like "Intl" for Interlization.
+     *
+     * The second parameter the "setView" accepts for zoom in/out, larger
+     * value for zoom in and smaller value for zoom out.
+     */
+    const map = L.map("map").setView(coords, 13);
+
+    /* The map we see on the page is basically made up of small tiles and the
+     * tiles come from the URL ("https://tile.openstreetmap.org/{z}/{x}/{y}.png" <-- openstreetmap
+     * is opensource map, leaflet works with all kinds of map, for e.g., googlemaps).
+     *
+     * We can also use that URL to change the apperance of the map
+     */
+    // L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords).addTo(map).bindPopup("You're currently here!").openPopup();
+  },
+  function () {
+    alert("Couldn't get your location!");
+  }
+);
